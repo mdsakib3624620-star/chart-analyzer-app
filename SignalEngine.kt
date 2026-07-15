ad3a52aba271c9f643b1a250d54b0f9b9ed7c080
mdsakib3624620-star <mdsakib3624620@gmail.com>
2026-07-15 21:13:25 +0600

    Create SignalEngine.kt

---
+++ SignalEngine.kt
@@ -0,0 +1,10 @@
+package com.example.chartanalyzer
+
+class SignalEngine {
+
+    fun generateSignal(): String {
+        // CALL / PUT signal logic will be added here
+        return "WAIT"
+    }
+
+}
