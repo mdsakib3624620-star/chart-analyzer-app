18bb5182eee37af30e17c08ce1209dbc6c3fd898
mdsakib3624620-star <mdsakib3624620@gmail.com>
2026-07-15 22:11:43 +0600

    Create CandleDetector.kt

---
+++ CandleDetector.kt
@@ -0,0 +1,16 @@
+package com.example.chartanalyzer
+
+class CandleDetector {
+
+    fun detect(): String {
+
+        return """
+        Candle Detection Ready
+        
+        Detecting:
+        - Body Size
+        - Wick Size
+        - Bullish/Bearish
+        """.trimIndent()
+    }
+}
