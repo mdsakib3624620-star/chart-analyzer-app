14a13e188f3e92cc7085a2c6270b8ba7a7676072
mdsakib3624620-star <mdsakib3624620@gmail.com>
2026-07-15 21:26:08 +0600

    Update CameraAnalyzer.kt

--- CameraAnalyzer.kt
+++ CameraAnalyzer.kt
@@ -2,8 +2,24 @@ package com.example.chartanalyzer
 
 class CameraAnalyzer {
 
-    fun startScan() {
-        // Camera live scan function will be added here
+    var isScanning = false
+
+    fun startScan(): String {
+        isScanning = true
+
+        return "Camera Scan Started"
+    }
+
+    fun stopScan(): String {
+        isScanning = false
+
+        return "Camera Scan Stopped"
+    }
+
+    fun analyzeImage(imagePath: String): String {
+        // Screenshot/Camera image analysis will be added here
+
+        return "Image received: $imagePath"
     }
 
 }
