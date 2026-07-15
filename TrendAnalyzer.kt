d787f63cc395eb203905f321fd3e933bd8067887
mdsakib3624620-star <mdsakib3624620@gmail.com>
2026-07-15 21:56:11 +0600

    Update TrendAnalyzer.kt

--- TrendAnalyzer.kt
+++ TrendAnalyzer.kt
@@ -2,14 +2,29 @@ package com.example.chartanalyzer
 
 class TrendAnalyzer {
 
-    fun detectTrend(
-        currentPrice: Double,
-        previousPrice: Double
-    ): String {
+    fun detectTrend(prices: List<Double>): String {
+
+        if (prices.size < 2) {
+            return "NO DATA"
+        }
+
+        var up = 0
+        var down = 0
+
+        for (i in 1 until prices.size) {
+
+            if (prices[i] > prices[i - 1]) {
+                up++
+            }
+
+            if (prices[i] < prices[i - 1]) {
+                down++
+            }
+        }
 
         return when {
-            currentPrice > previousPrice -> "UP"
-            currentPrice < previousPrice -> "DOWN"
+            up > down -> "UP"
+            down > up -> "DOWN"
             else -> "SIDEWAYS"
         }
     }
